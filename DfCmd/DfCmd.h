f8a5c7011fed0d8227fb4d0bf258d062797ca042
Yoshio HANAWA <y@hnw.jp>
2016-07-05 01:18:31 +0900

    Initial commit

---
+++ DfCmd/DfCmd.h
@@ -0,0 +1,19 @@
+//
+//  DfCmd.h
+//  DfCmd
+//
+//  Created by hnw on 2016/07/05.
+//  Copyright © 2016年 hnw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DfCmd.
+FOUNDATION_EXPORT double DfCmdVersionNumber;
+
+//! Project version string for DfCmd.
+FOUNDATION_EXPORT const unsigned char DfCmdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DfCmd/PublicHeader.h>
+
+
